ffcad0196879549f41a5b9bd9109a6ebb14a91cf
raiym2048 <raiymbek.daniiaruulu@iaau.edu.kg>
2021-12-20 11:30:17 +0600

    added

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -8,7 +8,10 @@ import java.net.Socket;
 public class Client {
     public static void main(String[] args){
         try (Phone phone = new Phone("127.0.0.1",9000)){
-           phone.writeLine("");
+            String request = phone.readLine();
+            System.out.println("the request is: " + request);
+
+            phone.writeLine("the request is: "+request);
         }
         catch (IOException e){
             throw new RuntimeException(e);
